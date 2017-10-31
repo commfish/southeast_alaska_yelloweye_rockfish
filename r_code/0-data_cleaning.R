5d6ba7045ac48992828b193fb70be0d5fcfda3a8
Ben Williams <ben.williams@alaska.gov>
2017-10-30 18:47:21 -0800

    update cleaning

--- r_code/0-data_cleaning.R
+++ r_code/0-data_cleaning.R
@@ -31,7 +31,7 @@ rocky <- c("3106", "3078", "3084", "3086", "3092", "3097",
            "3016", "3029", "3034", "3051", "3062", "3063", 
            "4027", "4019", "4018", "3113", "3110")
 
-read_csv("data/raw/iphc/IPHC_historic_survey_data.csv")  %>% filter(AREA=='SSEO')
+read_csv("data/raw/iphc/IPHC_historic_survey_data.csv")  %>% 
   filter(EFFECTIVE_SET=="Y") %>% 
   mutate(subset = ifelse(YEAR>2007 | is.na(SUBSAMPLE_CODE), 0, SUBSAMPLE_CODE),
          number = ifelse(subset==1, 
@@ -39,8 +39,37 @@ read_csv("data/raw/iphc/IPHC_historic_survey_data.csv")  %>% filter(AREA=='SSEO'
                          TOTAL_NUMBER),
          effort = EFFECTIVE_SKATE_VALUE * 100) %>% 
   filter(!is.na(YEAR), !is.na(STATION)) %>% 
-  dplyr::select(year = YEAR, effort, station = STATION, area = MANAGEMENT_AREA_CODE, number) %>% 
-  bind_rows(later_years) %>% arrange(-number)
+  dplyr::select(year = YEAR, 
+  				  effort, 
+  				  station = STATION, 
+  				  area = MANAGEMENT_AREA_CODE, number) %>% 
+  bind_rows(later_years) %>% 
+	arrange(number) %>% 
+	mutate(cpue = number / effort) %>% 
+	group_by(year, area) %>% 
+	summarise(var = var(cpue), cpue = mean(cpue)) %>% 
+	ggplot(aes(year, cpue, color=area)) + geom_point() + geom_line()
 
-  filter(station %in% rocky) %>% 
+# age comp ----
+
+age_comp <- read_csv('data/raw/fishery/age_comp_1985_2016.csv')
+
+age_comp %>% 
+	dplyr::select(year = YEAR, Area = G_MANAGEMENT_AREA_CODE, age = AGE,
+					  sex = SEX_CODE) %>% 
+	filter(!is.na(age)) %>% 
+	mutate(age = ifelse(age>75, 75, age), Year = factor(year)) %>% 
+	ggplot(aes(age, fill=Area))+geom_density(adjust=0.45,alpha=.2)+
+	xlab("Age")+
+	ylab("Age-class proportion")
+
+# age error matrix ----
+age_error <- read.csv('data/raw/fishery/age_error_1995_2014.csv')
+
+age_error %>% 
+	dplyr::select(age = AGE, Read = READABILITY_CODE, true = RELEASE_AUTHORITATIVE,
+					  Sample = SAMPLE_ID, specimen = SPECIMEN_ID, Sex = GENDER_CODE) %>% 
+	filter(!is.na(age), !Read %in% c("MO", "NO"), age<=75) %>% 
+	write_csv("data/fishery/age_error.csv")
+	
 
