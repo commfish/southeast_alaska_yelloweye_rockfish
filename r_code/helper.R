7fb9f8dd0621221b463500e8de8059f6ba8f7cac
Ben Williams <ben.williams@alaska.gov>
2017-10-30 18:47:35 -0800

    update helper

--- r_code/helper.R
+++ r_code/helper.R
@@ -5,3 +5,7 @@
 # load ----
 library(tidyverse)
 library(lubridate)
+
+theme_set(theme_bw(base_size=12)+ 
+			 	theme(panel.grid.major = element_blank(),
+			 			panel.grid.minor = element_blank()))
